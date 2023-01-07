8885ff5c894e6119ac786bc1858029d361299688
ClickyCodeDev <80412763+ClickyCodeDev@users.noreply.github.com>
2023-01-07 14:15:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # To lazy to write out a list?
 # place all items into the file things.txt, run the program and copy the output!
 names = []
-with open('PythonListMaker/things.txt') as open_file:
+with open('PythonListMaker/things.txt') as open_file: # Rename parent folder to PythonListMaker for this to work
     for line in open_file:
         names.append(line.strip())
 print(names)
